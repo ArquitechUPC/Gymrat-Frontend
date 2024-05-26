94134078817a4cad1d6b4f304bd23f8d638e7abb
GabrielMarllos <u202121584@upc.edu.pe>
2024-05-26 12:17:47 -0500

    Initial commit

---
+++ src/shared/http-common.js
@@ -0,0 +1,4 @@
+const AUTH_API_URL = import.meta.env.VITE_AUTH_API_URL;
+const CLASSES_API_URL = import.meta.env.VITE_CLASSES_API_URL;
+const CLIENTS_API_URL = import.meta.env.VITE_CLIENTS_API_URL;
+const PLANS_API_URL = import.meta.env.VITE_PLANS_API_URL;
