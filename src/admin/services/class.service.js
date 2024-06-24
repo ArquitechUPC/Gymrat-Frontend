b89f3e15b1142f15ae61379beb81f98d7a5d8d9c
RamiroVT <113039963+Ramiro-71@users.noreply.github.com>
2024-06-24 00:58:20 -0500

    Update class.service.js

--- src/admin/services/class.service.js
+++ src/admin/services/class.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/v1/classes';
+const API_URL = 'http://20.96.176.25:8080/api/v1/classes';
 
 class ClassService {
     getClasses() {
